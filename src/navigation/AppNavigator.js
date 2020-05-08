fb45e3b73f796c0a30ce0462b886e20485d1511a
Onur Özkaya <info@nrzky.com>
2020-05-08 21:23:07 +0300

    Search sayfası yapılandırıldı.

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { Home, Search } from '../views';
+import Colors from '../constants/Colors';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -10,7 +11,11 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: Colors.primary,
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={Home}
@@ -34,7 +39,11 @@ function TabNavigator() {
 function AppNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
--- src/views/Search.js
+++ src/views/Search.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Colors from '../constants/Colors';
 
 function Search() {
   return (
     <View style={styles.container}>
-      <Text>Search Tab</Text>
+      <View style={styles.headerView}>
+        <Text style={styles.titleText}>Deprem</Text>
+        <Text style={styles.subtitleText}>Arama Yap</Text>
+        <TextInput style={styles.textInput} />
+      </View>
+      <View style={styles.contentView}>
+
+      </View>
     </View>
   );
 }
@@ -14,7 +23,39 @@ export default Search;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerView: {
+    backgroundColor: Colors.primary,
+    height: 250,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: Colors.white,
+  },
+  subtitleText: {
+    fontSize: 16,
+    color: Colors.white,
+  },
+  textInput: {
+    backgroundColor: Colors.white,
+    padding: 20,
+    position: 'absolute',
+    width: 300,
+    bottom: -30,
+    alignSelf: 'center',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  contentView: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    zIndex: -1
+  }
 });
