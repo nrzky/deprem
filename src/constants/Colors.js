e176411e0ee31828e105fd4f88656f3f83fd6067
Onur Özkaya <info@nrzky.com>
2020-05-08 20:00:17 +0300

    Colors sabitleri oluşturuldu.

---
+++ src/constants/Colors.js
@@ -0,0 +1,7 @@
+const primary = "#fd5e53";
+const white = "#f9fcfb";
+const black = "#000";
+const light = "#b0eacd";
+const success = "#21bf73";
+
+export default { primary, white, black, light, success };
