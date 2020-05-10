0aa2b357eabca33448b2b9713673c0ecd58345c2
Onur Özkaya <info@nrzky.com>
2020-05-10 06:36:09 +0300

    Bölgeyi Google'da arama özelliği getirildi.

--- src/views/MapDetail.js
+++ src/views/MapDetail.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Linking, Text, Dimensions, StyleSheet } from 'react-native';
 import Colors from '../constants/Colors';
 import { IconButton } from '../components';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 function MapDetail({ route, navigation }) {
+  searchGoogle = () => {
+    Linking.openURL(`https://www.google.com/search?q=${route.params.item.title}`)
+  }
   return (
     <View>
       <View style={styles.headerView}>
@@ -17,6 +20,14 @@ function MapDetail({ route, navigation }) {
 
         <Text style={styles.titleText}>Deprem Haritası</Text>
         <Text style={styles.subtitleText}>{route.params.item.title}</Text>
+        <View style={[styles.buttonView]}>
+          <IconButton
+            onPress={searchGoogle}
+            name="map-search"
+            color={Colors.success}
+            size={40}
+          />
+        </View>
       </View>
       <View style={styles.contentView}>
         <MapView
@@ -30,8 +41,8 @@ function MapDetail({ route, navigation }) {
           }}
         >
           <Marker
-          title={route.params.item.title}
-          description={`Deprem Şiddeti: ${route.params.item.mag}`}
+            title={route.params.item.title}
+            description={`Deprem Şiddeti: ${route.params.item.mag}`}
             coordinate={{
               latitude: route.params.item.lat,
               longitude: route.params.item.lng
@@ -66,11 +77,27 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: Colors.white,
   },
+  buttonView: {
+    flexDirection: 'row',
+    backgroundColor: Colors.white,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    position: 'absolute',
+    bottom: -40,
+    alignItems: 'center',
+    borderRadius: 100,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 5,
+  },
   contentView: {
     height: Dimensions.get('window').height - 250,
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
+    zIndex: -1,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
